44ac5e060e7a17b7636b8f29aff5a427eb738ccb
CimimUxMaio <cimimuxmaio@gmail.com>
2021-05-18 19:23:16 -0300

    UserNotConnectedToVoiceChannel exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -5,7 +5,7 @@ class ModelException(Exception):
 
 class MissingParameter(ModelException):
     def __init__(self, param_name):
-        super().__init__(f"{param_name} is a required argument that is missing.")
+        super().__init__(f"{param_name} is a required argument that is missing")
         
 
 class LastUninterpretedCommandMessageMissing(ModelException):
@@ -20,8 +20,12 @@ class InvalidClassification(ModelException):
 
 class InvalidSongName(ModelException):
     def __init__(self, song_name):
-        super().__init__(f"Could not download the song: {song_name}. Incorrect format try another. This could be due to it being in playlist or livestream format.")
+        super().__init__(f"Could not download the song: {song_name}. Incorrect format try another. This could be due to it being in playlist or livestream format")
 
+
+class UserNotConnectedToVoiceChannel(ModelException):
+    def __init__(self):
+        super().__init__(f"You must be connected to a voice channel")
 
 
 async def handle_model_error(ctx, error):
