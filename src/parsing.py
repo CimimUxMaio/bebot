e08ca8f2682416c14b8952d7c258e08d2024b1c0
CimimUxMaio <cimimuxmaio@gmail.com>
2021-05-19 12:59:21 -0300

    Fix recursion params detection

--- src/parsing.py
+++ src/parsing.py
@@ -5,7 +5,12 @@ def params(message):
     if len(message) == 0 or open_i == -1:
         return []
 
-    close_i = message[open_i:].find("]") + open_i
+    relative_close_i = message[open_i:].find("]")
+    if relative_close_i == -1:
+        return []
+
+    close_i = relative_close_i + open_i
+
     param = message[open_i+1:close_i]
     return [param] + params(message[close_i+1:])
 
