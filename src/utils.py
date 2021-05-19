d51f6eed872e9daac72d0cf268d492f8a6c644c7
CimimUxMaio <cimimuxmaio@gmail.com>
2021-05-19 01:56:19 -0300

    utils.embeded_message: color + blame

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
 import discord
 
 
-async def embeded_message(ctx, *, action=None, message, color=discord.Colour.blue()):
+async def embeded_message(ctx, *, action=None, message, color=discord.Colour.blue(), blame: discord.Member=None):
     premessage = f"**{action}:** " if action else ""
     complete_message = premessage + message
-    embed = discord.Embed(description=complete_message) 
+    embed = discord.Embed(description=complete_message, color=color) 
+
+    if blame:
+        embed.set_footer(blame.mention)
+        
     await ctx.send(embed=embed)
