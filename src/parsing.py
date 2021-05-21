78c9ed3f03fc9ba6580669ad73b646a3602909bf
CimimUxMaio <cimimuxmaio@gmail.com>
2021-05-21 02:07:17 -0300

    Flexible param wrappers + now param wrappers are " "

--- src/parsing.py
+++ src/parsing.py
@@ -1,21 +1,24 @@
 import re
+import config
+
+OPENER = '\"'
+CLOSER = '\"'
 
 def params(message):
-    open_i = message.find("[")
+    open_i = message.find(OPENER)
     if len(message) == 0 or open_i == -1:
         return []
 
-    relative_close_i = message[open_i:].find("]")
-    if relative_close_i == -1:
+    relative_close_i = message[open_i+1:].find(CLOSER) + 1
+    if relative_close_i == 0:
         return []
 
     close_i = relative_close_i + open_i
-
     param = message[open_i+1:close_i]
     return [param] + params(message[close_i+1:])
 
 
 def parse_message(message):
     parameters = params(message)
-    command_message = re.sub('\[(.*)\]', '', message).strip()
+    command_message = re.sub(f'\{OPENER}(.*)\{CLOSER}', '', message).strip()
     return command_message, parameters
